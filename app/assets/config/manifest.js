7924bd13685b87313d290ad9ae0237bed0f98e1b
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-05-31 13:52:49 +0200

    Add Bootstrap tp rails project

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+// = link_tree ../javascript .js
+// = link_tree ../../../vendor/javascript .js
+//= require application
+// = link application.css
+// = link users/show.css
+// = link posts/show.css
