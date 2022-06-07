cbd2dc7b23e3a5fdc64364987e946f985c953416
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-07 12:12:58 +0200

    Add auto increment to Post. Likes_Counter after saving new Like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'AuthorId_id'
   belongs_to :post, foreign_key: 'PostId_id'
+
+  after_save do
+    post.increment!(:Likes_Counter)
+  end
 end
