d4c69030eb75317b7fa4ec2d2d765b73bf9cbbbe
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-13 14:32:19 +0200

    Add validation for Comment model creation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'AuthorId_id'
   belongs_to :post, foreign_key: 'PostId_id'
 
+  validates :text, :AuthorId_id, :PostId_id, presence: true
+
   after_save do
     post.increment!(:Comments_Counter)
   end
