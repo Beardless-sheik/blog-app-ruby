444257fcce03d84d62b2d11c28496dfce89ad634
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-14 13:49:03 +0200

    Add includes in Post and User controller to eager load associations and fix N+1 problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
     @current_user = @user
   end
 
   def show
-    @post = Post.find(params[:post_id])
+    @post = Post.includes(:comments).find(params[:post_id])
   end
 
   def new
