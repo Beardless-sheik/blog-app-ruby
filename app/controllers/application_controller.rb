88af5ae92967c56a11dfbd393cf1f8163448ede7
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-06-22 15:22:22 +0300

    modfiy the view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  # before_action :authenticate_user!
+
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:Name])
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :name, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :name, :password, :current_password)
+    end
   end
 end
