d984d514a7b1f040bb4fb12b6879de4a17ac6775
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-22 16:00:42 +0200

    Add before hook to authenticate on all routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  # before_action :authenticate_user!
-
+  before_action :authenticate_user!
+  
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:email, :name, :password, :password_confirmation)
-    end
-
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:email, :name, :password, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:Name])
   end
 end
