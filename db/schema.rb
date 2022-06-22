0607ec5f1d3e3a61dde44679ca52de13860d91ea
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-22 16:00:18 +0200

    Add confirmable module attributes to the data migration

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_124109) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
   end
 
   add_foreign_key "comments", "posts", column: "PostId_id"
